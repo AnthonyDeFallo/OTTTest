1b9a119589759ae8cce992f71794ace4985e362b
Anthony DeFallo <tekenology@gmail.com>
2021-03-17 18:40:42 +0000

    Config Fix

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export default {
+  accountId: 'FRANUATH', // <-- find this from BoxCast Support
+  channelId: '0sQPtletSML5kwVzKobh', // <-- find this on your boxcast.tv global account page
+  channelFilter: (c) => {
+    return true  // <-- can use this to exclude certain channels from left nav
+  }
+}
