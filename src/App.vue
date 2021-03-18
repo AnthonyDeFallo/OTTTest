e1116aae216c775049f7ec3ff1f0f79baf15ce17
Anthony DeFallo <tekenology@gmail.com>
2021-03-18 00:28:17 +0000

    Update Color of Header

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #21412a;
   margin-top: 15px;
 }
 .ellipse, .jumbotron h1 {
