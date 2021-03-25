b158413644eed7a948205da678da5034aa3d2ed5
Anthony DeFallo <tekenology@gmail.com>
2021-03-25 14:28:36 -0400

    Exclude channels

--- src/config.js
+++ src/config.js
@@ -2,6 +2,8 @@ export default {
   accountId: 'FRANUATH', // <-- find this from BoxCast Support
   channelId: '0sQPtletSML5kwVzKobh', // <-- find this on your boxcast.tv global account page
   channelFilter: (c) => {
-    return true  // <-- can use this to exclude certain channels from left nav
+    ['bhfcflhlnm1vdejaktgs', 'mmxbmquk0ronyfucfk9w']
+    
+    //return true  // <-- can use this to exclude certain channels from left nav
   }
 }
