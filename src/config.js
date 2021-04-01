6b04295e24c289f6e20193818ec163fb6df28ad6
Anthony DeFallo <tekenology@gmail.com>
2021-04-01 17:12:26 -0400

    Change tabs

--- src/config.js
+++ src/config.js
@@ -10,6 +10,9 @@ export default {
   // irlcwdutrm4zlbbyvgwl Athletics
   // mmxbmquk0ronyfucfk Orientation
   // e2hvomftjkoxtzmjm0pt FOPs
-  channelFilter: (c) => ['irlcwdutrm4zlbbyvgwl', 'mmxbmquk0ronyfucfk9w', 'e2hvomftjkoxtzmjm0pt'].indexOf(c.id) < 0,
+  // pcgcwum4wsnj6tnmxhr2 Commercials
+  // yvytn8banbiudvxfanu1 FranU Online
+  // h45soqlh2dqbbqmdqgjk Academic Events
+  channelFilter: (c) => ['irlcwdutrm4zlbbyvgwl', 'mmxbmquk0ronyfucfk9w', 'e2hvomftjkoxtzmjm0pt', 'h45soqlh2dqbbqmdqgjk', 'yvytn8banbiudvxfanu1', 'pcgcwum4wsnj6tnmxhr2'].indexOf(c.id) < 0,
   
 }
